701631fabe61a3013cfa9d22d0cffc920e9a91d8
David Jedelsky <david@swissmicros.com>
2018-05-30 22:12:58 +0200

    Fix include dir

--- common/core_aux.cc
+++ common/core_aux.cc
@@ -402,7 +402,7 @@ void core_set_printer_delay(unsigned int val) {
 }
 
 // Temporary version hack - Version info isn't available in Free42 interface :(
-#include "../../free42/VERSION.rc"
+#include "../VERSION.rc"
 
 const char * free42_version_str() {
   return FREE42_VERSION_1;
